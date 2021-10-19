6ee5110cc0102df1cf99114806fc96bf1efe4e96
Daniel Way <contact@danieldway.com>
2021-10-18 21:06:15 -0500

    feat(form): implement basic render logic using schema structure

--- src/form.rs
+++ src/form.rs
@@ -1,3 +1,14 @@
+use tty_interface::interface::TTYInterface;
+use crate::schema::Schema;
+
+extern crate termion;
+
+use termion::raw::IntoRawMode;
+use termion::event::Key;
+use termion::input::TermRead;
+
+use std::io::stdin;
+
 pub struct TTYForm {
 	// TODO: reader/writer
 }
@@ -8,5 +19,34 @@ impl TTYForm {
 		TTYForm {}
 	}
 
-	pub fn render(/* TODO: schema */) /* TODO: data */ {}
+	pub fn render(self, mut schema: Schema) /* TODO: data */ {
+    	let stdin = stdin();
+    	let mut stdout = std::io::stdout().into_raw_mode().unwrap();
+    	let mut tty = TTYInterface::new(&mut stdout);
+
+		for wrapped_key in stdin.keys() {
+			let key = wrapped_key.unwrap();
+			match key {
+				Key::Char(ch) => {
+					if ch == 'q' {
+						break;
+					}
+
+					schema.handle_input(key);
+					let mut lines = schema.render();
+
+					let mut batch = tty.start_update();
+					let mut line_index = 0;
+					while !lines.is_empty() {
+						batch.set_line(line_index, lines.remove(0));
+						line_index += 1;
+					}
+					tty.perform_update(batch).unwrap();
+				},
+				_ => {},
+			}
+		}
+
+		tty.end().unwrap();
+	}
 }
